d02759ad35a82538cbd220575d5d9c6cdd24fbc7
georgij <crystarm@yandex.ru>
2025-10-23 11:49:18 +0300

    UI, interactive mode and all the stuff

--- include/pkgdiff.h
+++ include/pkgdiff.h
@@ -1,16 +1,12 @@
 #ifndef PKGDIFF_H
 #define PKGDIFF_H
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-void hello_pkgdiff();
+void hello_pkgdiff(void);
 char *fetch_packages_json(const char *branch);
-void json_parse_demo(const char *json_data);
-
+void compare_branches_interactive_min(const char *branch1, const char *branch2, const char *arch_filter);
 #ifdef __cplusplus
 }
 #endif
-
-#endif // PKGDIFF_H
+#endif
